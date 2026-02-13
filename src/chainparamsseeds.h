fba1c192a26b580b6c082cb99ebcf0f97eaddd8a
moyashi170607 <takayaokada0607@gmail.com>
2026-02-13 09:19:19 +0900

    仮置き

--- src/chainparamsseeds.h
+++ src/chainparamsseeds.h
@@ -1,3 +1,4 @@
+#include <cstdint>
 #ifndef BITCOIN_CHAINPARAMSSEEDS_H
 #define BITCOIN_CHAINPARAMSSEEDS_H
 /**
@@ -6,11 +7,11 @@
  *
  * Each line contains a BIP155 serialized (networkID, addr, port) tuple.
  */
-static const uint8_t chainparams_seed_main[] = {};
+static const uint8_t chainparams_seed_main[] = {0x00};
 
-static const uint8_t chainparams_seed_signet[] = {};
+static const uint8_t chainparams_seed_signet[] = {0x00};
 
-static const uint8_t chainparams_seed_test[] = {};
+static const uint8_t chainparams_seed_test[] = {0x00};
 
-static const uint8_t chainparams_seed_testnet4[] = {};
+static const uint8_t chainparams_seed_testnet4[] = {0x00};
 #endif  // BITCOIN_CHAINPARAMSSEEDS_H
